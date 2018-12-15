c7437c3f5a8c7763333f8b06980865c43a38caa0
Tyler Milner <tyler.milner@bottlerocketstudios.com>
2018-12-15 11:17:54 -0600

    Implemented a data table for multiple shouters scenario.

--- src/test/java/shouty/ShoutSteps.java
+++ src/test/java/shouty/ShoutSteps.java
@@ -3,6 +3,7 @@ package shouty;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import java.util.List;
 
 import static java.util.Collections.emptyMap;
 import static org.junit.Assert.assertEquals;
@@ -35,5 +36,19 @@ public class ShoutSteps {
     @Then("Lucy should not hear Oscar")
     public void lucyShouldNotHearOscar() {
         assertEquals(shouty.getShoutsHeardBy("Lucy").toString(), "{Sean=[Hello, world]}");
+    }
+
+    @Given("people are located at")
+    public void peopleAreLocatedAt(List<List<String>> dataTable) {
+        for (int i = 1; i < dataTable.size(); i++) {
+            List<String> row = dataTable.get(i);
+
+            String name = row.get(0);
+            int xCoord = new Integer(row.get(1)).intValue();
+            int yCoord = new Integer(row.get(2)).intValue();
+
+            Coordinate location = new Coordinate(xCoord, yCoord);
+            shouty.setLocation(name, location);
+        }
     }
 }
